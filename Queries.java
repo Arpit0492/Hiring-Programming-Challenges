1f34064eb6e5b48d91189e437e3799e4f5a75bbb
Arpit Johri <johri.arpit@gmail.com>
2017-01-08 01:49:47 +0530

    Add files via upload

---
+++ Queries.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.util.*;
+
+public class Queries {
+
+
+    public static void main(String[] args) {
+        MyScanner scan = new MyScanner();
+        PrintWriter out = new PrintWriter(new BufferedOutputStream(System.out), true);
+
+        int N = scan.nextInt();
+        for (int i = 0; i <N; i++) {
+            int A = scan.nextInt();
+
+            ArrayList<Integer> subSpecial = getSubSpecialSummation(A);
+
+
+            for (int j = A+1; ; j++) {
+                ArrayList<Integer> nextList = getSubSpecialSummation(j);
+
+                if (nextList.containsAll(subSpecial)) {
+                    out.println(j);
+                    break;
+                }
+
+            }
+
+        }
+
+        out.flush();
+        out.close();
+
+
+    }
+
+    private static ArrayList<Integer> getSubSpecialSummation(int A) {
+        int val;
+        ArrayList<Integer> list = new ArrayList<>();
+        while (A!=0){
+            val=getPreviousPowerOfTwo(A);
+            A-=val;
+            list.add(val);
+        }
+
+        return list;
+    }
+
+    private static int getPreviousPowerOfTwo(int size)
+    {
+        int n = -1;
+        while (size >> ++n > 0);
+        return 1 << n - 1;
+    }
+
+}
